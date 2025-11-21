2c520d8852be23d9528ce03fe86aeb0443c55ccf
chocomintw <cacaproutnews@gmail.com>
2025-11-21 20:57:43 +0100

    fix for vercel instead??

--- next.config.ts
+++ next.config.ts
@@ -1,13 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
- output: 'export',
-  trailingSlash: true,
-  images: {
-    unoptimized: true
-  },
-  basePath: process.env.NODE_ENV === 'production' ? '/visualrecords' : '', // Replace with your repo name
-  assetPrefix: process.env.NODE_ENV === 'production' ? '/visualrecords/' : '', // Replace with your repo name
+
 };
 
 export default nextConfig;
